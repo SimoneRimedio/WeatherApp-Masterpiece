786b26a3470d9d8e612382f852e7343e440162d0
SimoneRimedio <rimediosimone1@gmail.com>
2024-04-17 18:59:16 +0200

    description.json added, minor changes on css

--- src/App.tsx
+++ src/App.tsx
@@ -42,13 +42,17 @@ const App = (): ReactElement => {
 
   return (
     <>
-      <h1>Citta: {currentLocation}</h1>
-      <form className='flex flex-col gap-2 max-w-40' onSubmit={handleLocation}>
-        <label>Inserisci una citta:</label>
-        <input type='text' className='border-2 border-gray-600 rounded-md' value={currentLocation} onChange={handleInput} /> 
-        <button type='submit' className='border-2 border-gray-600 rounded-md'>Submit</button>
-      </form>
+    <form className='flex max-w-md px-4 mx-auto mt-12' onSubmit={handleLocation}>
+      <input type='text' className='w-60 p-3 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600' value={currentLocation} onChange={handleInput} /> 
+      <button type='submit' className='border-1 border-gray-600 rounded-md relative'>
+        <svg xmlns="http://www.w3.org/2000/svg" className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+        </svg>
+      </button>
+    </form>
+    <div className="flex justify-center mt-10">
       {weatherData && <Card elements={weatherData.current}></Card>}
+    </div>
     </>
   );
 };
